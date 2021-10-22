18f8ed3ffed946c7950b791c1db5ecc3adde41d2
Cristopher Torres <cristopher.tmar@gmail.com>
2021-10-22 11:16:36 -0500

    211021

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,8 +1,8 @@
 // export const URL_SERVICIOS = 'https://localhost:5001';
-/* export const URL_SERVICIOS = 'https://localhost:44380';
+export const URL_SERVICIOS = 'https://localhost:44380';
 export const URL_IMG = 'https://localhost:44380/'; 
-export const URL_PAGINA = 'http://localhost:4200' */
+export const URL_PAGINA = 'http://localhost:4200'
 
-export const URL_SERVICIOS = 'https://transitlox.com';
+/* export const URL_SERVICIOS = 'https://transitlox.com';
 export const URL_IMG = 'https://transitlox.com/';
-export const URL_PAGINA = 'https://autosypropiedades.com'
+export const URL_PAGINA = 'https://autosypropiedades.com' */
